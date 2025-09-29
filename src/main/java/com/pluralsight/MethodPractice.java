dfe97c2d333db396c99f25969097848c131d1066
Vincentacheampong <Vincentacheampong10@gmail.com>
2025-09-29 13:40:08 -0400

    Method Practice

---
+++ src/main/java/com/pluralsight/MethodPractice.java
@@ -0,0 +1,31 @@
+package com.pluralsight;
+
+
+
+public class MethodPractice {
+
+    public static void main(String[] args) {
+        sayHi();
+        dance();
+        fish();
+    }
+
+
+    public static void sayHi() {
+        System.out.println("Hi");
+    }
+
+    public static void dance(){
+        System.out.println("WoahhhhhOhhhhhh");
+        System.out.println("WoahhhhhOhhhhhh");
+        System.out.println("Stop");
+        System.out.println("Hammer time!");
+    }
+
+    public static void fish(){
+        System.out.println("Get out fishing rod");
+        System.out.println("Bait the hook");
+        System.out.println("Cast");
+        System.out.println("Wait.....forever");
+    }
+}
